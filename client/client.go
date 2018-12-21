4017cf1e7ff10950217e7409d00bec3b2893d2c3
code-to-gold <jayden.park@paust.io>
2018-12-21 11:44:28 +0900

    Run go fmt because of indent

--- client/client.go
+++ client/client.go
@@ -1,11 +1,10 @@
 package client
 
 import (
-
+	"bufio"
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
-    "bufio"
 	"github.com/paust-team/paust-db/types"
 	"github.com/spf13/cobra"
 	"github.com/tendermint/tendermint/rpc/client"
