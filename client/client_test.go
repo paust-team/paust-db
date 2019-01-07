4e1f016b2df85a26a35497cd29cd10a1ffddf1d8
Kevin <kevin.choi@paust.io>
2019-01-07 13:29:30 +0900

    Split client's unit test by functionality
    유지보수의 편리함을 위해 하나의 파일로 작성된 unit test를
    read, write로 분리하여 파일을 새롭게 생성.
    매번 테스트에서 dbClient를 새로 생성하므로 ClientTestSuite 내에
    dbClient를 추가하고 SetupTest에서 초기화하도록 변경.
    testify 패키지의 require와 assert를 각 특징에 맞게 사용하도록 변경.

--- client/client_test.go
+++ client/client_test.go
@@ -1,22 +1,14 @@
 package client_test
 
 import (
-	"encoding/base64"
-	"encoding/json"
 	"github.com/paust-team/paust-db/client"
 	"github.com/paust-team/paust-db/master"
-	"github.com/paust-team/paust-db/types"
 	"github.com/stretchr/testify/suite"
-	abci "github.com/tendermint/tendermint/abci/types"
 	cmn "github.com/tendermint/tendermint/libs/common"
 	nm "github.com/tendermint/tendermint/node"
-	rpcClient "github.com/tendermint/tendermint/rpc/client"
 	"github.com/tendermint/tendermint/rpc/test"
-	tendermint "github.com/tendermint/tendermint/types"
-	"io/ioutil"
 	"os"
 	"testing"
-	"time"
 )
 
 var node *nm.Node
@@ -29,6 +21,8 @@ const (
 
 type ClientTestSuite struct {
 	suite.Suite
+
+	dbClient *client.Client
 }
 
 func (suite *ClientTestSuite) SetupSuite() {
@@ -44,83 +38,8 @@ func (suite *ClientTestSuite) TearDownSuite() {
 	os.RemoveAll(testDir)
 }
 
-func (suite *ClientTestSuite) TestClient_WriteData() {
-	mempool := node.MempoolReactor().Mempool
-	initMempoolSize := mempool.Size()
-
-	time := time.Now()
-	data := []byte(cmn.RandStr(8))
-	pubKeyBytes, _ := base64.StdEncoding.DecodeString(TestPubKey)
-	tx, _ := json.Marshal(types.DataSlice{types.Data{Timestamp: time.UnixNano(), UserKey: pubKeyBytes, Type: TestDataType, Data: data}})
-
-	dbClient := client.NewLocalClient(node)
-	bres, err := dbClient.WriteData(time, TestPubKey, TestDataType, data)
-
-	suite.Nil(err, "err: %+v", err)
-	suite.Equal(bres.Code, abci.CodeTypeOK)
-
-	suite.Equal(initMempoolSize+1, mempool.Size())
-
-	txs := mempool.ReapMaxTxs(-1)
-	suite.Equal(tendermint.Tx(tx), txs[0])
-
-	mempool.Flush()
-}
-
-func (suite *ClientTestSuite) TestClient_ReadData() {
-	time := time.Now()
-	data := []byte(cmn.RandStr(8))
-	pubKeyBytes, _ := base64.StdEncoding.DecodeString(TestPubKey)
-	tx, _ := json.Marshal(types.DataSlice{types.Data{Timestamp: time.UnixNano(), UserKey: pubKeyBytes, Type: TestDataType, Data: data}})
-
-	c := rpcClient.NewLocal(node)
-	_, err := c.BroadcastTxCommit(tx)
-	suite.Nil(err, "err: %+v", err)
-
-	dbClient := client.NewLocalClient(node)
-	res, err := dbClient.ReadData(time.UnixNano(), time.UnixNano()+1, TestPubKey, TestDataType)
-	qres := res.Response
-	if suite.Nil(err) && suite.True(qres.IsOK()) {
-		suite.EqualValues(tx, qres.Value)
-	}
-}
-
-func (suite *ClientTestSuite) TestClient_ReadMetaData() {
-	time := time.Now()
-	data := []byte(cmn.RandStr(8))
-	pubKeyBytes, _ := base64.StdEncoding.DecodeString(TestPubKey)
-	tx, _ := json.Marshal(types.DataSlice{types.Data{Timestamp: time.UnixNano(), UserKey: pubKeyBytes, Type: TestDataType, Data: data}})
-	expectedValue, _ := json.Marshal(types.MetaResponseSlice{types.MetaResponse{Timestamp: time.UnixNano(), UserKey: pubKeyBytes, Type: TestDataType}})
-
-	c := rpcClient.NewLocal(node)
-	_, err := c.BroadcastTxCommit(tx)
-	suite.Nil(err, "err: %+v", err)
-
-	dbClient := client.NewLocalClient(node)
-	res, err := dbClient.ReadMetaData(time.UnixNano(), time.UnixNano()+1, TestPubKey, TestDataType)
-	qres := res.Response
-	if suite.Nil(err) && suite.True(qres.IsOK()) {
-		suite.EqualValues(expectedValue, qres.Value)
-	}
-}
-
-func (suite *ClientTestSuite) TestClient_WriteFile() {
-	mempool := node.MempoolReactor().Mempool
-	initMempoolSize := mempool.Size()
-
-	dbClient := client.NewLocalClient(node)
-	bres, err := dbClient.WriteFile("../test/writeFile.json")
-	bytes, err := ioutil.ReadFile("../test/writeFile.json")
-
-	suite.Nil(err, "err: %+v", err)
-	suite.Equal(bres.Code, abci.CodeTypeOK)
-
-	suite.Equal(initMempoolSize+1, mempool.Size())
-
-	txs := mempool.ReapMaxTxs(-1)
-	suite.Equal(tendermint.Tx(bytes), txs[0])
-
-	mempool.Flush()
+func (suite *ClientTestSuite) SetupTest() {
+	suite.dbClient = client.NewLocalClient(node)
 }
 
 func TestClient(t *testing.T) {
---
+++ client/client_write_test.go
@@ -0,0 +1,61 @@
+package client_test
+
+import (
+	"encoding/base64"
+	"encoding/json"
+	"github.com/paust-team/paust-db/types"
+	"github.com/stretchr/testify/require"
+	abci "github.com/tendermint/tendermint/abci/types"
+	cmn "github.com/tendermint/tendermint/libs/common"
+	tendermint "github.com/tendermint/tendermint/types"
+	"io/ioutil"
+	"time"
+)
+
+func (suite *ClientTestSuite) TestClient_WriteData() {
+	require := require.New(suite.T())
+
+	mempool := node.MempoolReactor().Mempool
+	initMempoolSize := mempool.Size()
+
+	time := time.Now()
+	data := []byte(cmn.RandStr(8))
+	pubKeyBytes, err := base64.StdEncoding.DecodeString(TestPubKey)
+	require.Nil(err, "base64 decode err: %+v", err)
+	tx, err := json.Marshal(types.DataSlice{types.Data{Timestamp: time.UnixNano(), UserKey: pubKeyBytes, Type: TestDataType, Data: data}})
+	require.Nil(err, "json marshal err: %+v", err)
+
+	bres, err := suite.dbClient.WriteData(time, TestPubKey, TestDataType, data)
+
+	require.Nil(err, "err: %+v", err)
+	require.Equal(bres.Code, abci.CodeTypeOK)
+
+	require.Equal(initMempoolSize+1, mempool.Size())
+
+	txs := mempool.ReapMaxTxs(-1)
+	require.EqualValues(tendermint.Tx(tx), txs[0])
+
+	mempool.Flush()
+}
+
+func (suite *ClientTestSuite) TestClient_WriteFile() {
+	require := require.New(suite.T())
+
+	mempool := node.MempoolReactor().Mempool
+	initMempoolSize := mempool.Size()
+
+	bytes, err := ioutil.ReadFile("../test/writeFile.json")
+	require.Nil(err, "file read err: %+v", err)
+
+	bres, err := suite.dbClient.WriteFile("../test/writeFile.json")
+
+	require.Nil(err, "err: %+v", err)
+	require.Equal(bres.Code, abci.CodeTypeOK)
+
+	require.Equal(initMempoolSize+1, mempool.Size())
+
+	txs := mempool.ReapMaxTxs(-1)
+	require.EqualValues(tendermint.Tx(bytes), txs[0])
+
+	mempool.Flush()
+}
