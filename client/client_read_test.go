2a7e9f18796489cbaf0566532e1e634bb5da1744
Kevin <kevin.choi@paust.io>
2019-01-07 13:29:30 +0900

    Split client's unit test by functionality
    유지보수의 편리함을 위해 하나의 파일로 작성된 unit test를
    read, write로 분리하여 파일을 새롭게 생성.
    매번 테스트에서 dbClient를 새로 생성하므로 ClientTestSuite 내에
    dbClient를 추가하고 SetupTest에서 초기화하도록 변경.
    testify 패키지의 require와 assert를 각 특징에 맞게 사용하도록 변경.

---
+++ client/client_read_test.go
@@ -0,0 +1,67 @@
+package client_test
+
+import (
+	"encoding/base64"
+	"encoding/json"
+	"github.com/paust-team/paust-db/types"
+	"github.com/stretchr/testify/require"
+	cmn "github.com/tendermint/tendermint/libs/common"
+	rpcClient "github.com/tendermint/tendermint/rpc/client"
+	"time"
+)
+
+func (suite *ClientTestSuite) TestClient_ReadData() {
+	require := require.New(suite.T())
+
+	mempool := node.MempoolReactor().Mempool
+	time := time.Now()
+	data := []byte(cmn.RandStr(8))
+	pubKeyBytes, err := base64.StdEncoding.DecodeString(TestPubKey)
+	require.Nil(err, "base64 decode err: %+v", err)
+	tx, err := json.Marshal(types.DataSlice{types.Data{Timestamp: time.UnixNano(), UserKey: pubKeyBytes, Type: TestDataType, Data: data}})
+	require.Nil(err, "json marshal err: %+v", err)
+
+	c := rpcClient.NewLocal(node)
+	bres, err := c.BroadcastTxCommit(tx)
+
+	require.Nil(err, "err: %+v", err)
+	require.True(bres.CheckTx.IsOK())
+	require.True(bres.DeliverTx.IsOK())
+
+	require.Equal(0, mempool.Size())
+
+	res, err := suite.dbClient.ReadData(time.UnixNano(), time.UnixNano()+1, TestPubKey, TestDataType)
+	qres := res.Response
+	if suite.Nil(err) && suite.True(qres.IsOK()) {
+		suite.EqualValues(tx, qres.Value)
+	}
+}
+
+func (suite *ClientTestSuite) TestClient_ReadMetaData() {
+	require := require.New(suite.T())
+
+	mempool := node.MempoolReactor().Mempool
+	time := time.Now()
+	data := []byte(cmn.RandStr(8))
+	pubKeyBytes, err := base64.StdEncoding.DecodeString(TestPubKey)
+	require.Nil(err, "base64 decode err: %+v", err)
+	tx, err := json.Marshal(types.DataSlice{types.Data{Timestamp: time.UnixNano(), UserKey: pubKeyBytes, Type: TestDataType, Data: data}})
+	require.Nil(err, "json marshal err: %+v", err)
+	expectedValue, err := json.Marshal(types.MetaResponseSlice{types.MetaResponse{Timestamp: time.UnixNano(), UserKey: pubKeyBytes, Type: TestDataType}})
+	require.Nil(err, "json marshal err: %+v", err)
+
+	c := rpcClient.NewLocal(node)
+	bres, err := c.BroadcastTxCommit(tx)
+
+	require.Nil(err, "err: %+v", err)
+	require.True(bres.CheckTx.IsOK())
+	require.True(bres.DeliverTx.IsOK())
+
+	require.Equal(0, mempool.Size())
+
+	res, err := suite.dbClient.ReadMetaData(time.UnixNano(), time.UnixNano()+1, TestPubKey, TestDataType)
+	qres := res.Response
+	if suite.Nil(err) && suite.True(qres.IsOK()) {
+		suite.EqualValues(expectedValue, qres.Value)
+	}
+}
