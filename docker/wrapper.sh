d4cd87e6dd40af65158518f004b564d7e6b8522e
주경운 <33068618+kyungun@users.noreply.github.com>
2019-02-12 16:51:00 +0900

    Add deploy script and change directory of wrapper.sh in container from /usr/bin/wrapper.sh to /root/wrapper.sh
    docker directory에 localnet용 directory를 생성 후 localnet 관련 파일 이동
    최상위 directory의 Dockerfile을 배포용으로 수정

--- docker/wrapper.sh
+++ docker/wrapper.sh
@@ -3,17 +3,9 @@
 ##
 ## Input parameters
 ##
-ID=${ID:-0}
-LOG=${LOG:-tendermint.log}
 TENDERMINT=$GOPATH/bin/tendermint
 PAUSTDB=$GOPATH/bin/paust-db
 
-export TMHOME="/tendermint/node${ID}"
+$PAUSTDB master -d $TMHOME &
 
-nohup $PAUSTDB master -d $TMHOME & > "${TMHOME}/paust-db.log"
-
-if [ -d "`dirname ${TMHOME}/${LOG}`" ]; then
-  $TENDERMINT $@ | tee "${TMHOME}/${LOG}"
-else
-  $TENDERMINT $@
-fi
+$TENDERMINT $@
