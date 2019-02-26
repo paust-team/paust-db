6b9a5746c8cdd7c9582372c127aec8df479c9f7a
kwjooo <andrew.joo@paust.io>
2019-02-26 15:09:38 +0900

    Delete useless function which was used only once
    types/types.go에 존재하던 CreateStartByteAndEndByte를 삭제하고 함수 사용처에 로직 구현
    해당 함수가 재사용성이 없을 것으로 판단되며 master에서만 사용하는 function이므로 types.go에 존재하는 것은 옳지 않음

--- types/types.go
+++ types/types.go
@@ -1,11 +1,5 @@
 package types
 
-import (
-	"encoding/binary"
-	"encoding/json"
-	"fmt"
-)
-
 //TODO offset 추가
 type KeyObj struct {
 	Timestamp []byte `json:"timestamp"`
@@ -37,24 +31,4 @@ type QueryObj struct {
 
 type FetchObj struct {
 	RowKeys [][]byte `json:"rowKeys"`
-}
-
-// 주어진 DataQuery로부터 시작할 지점(startByte)과 마지막 지점(endByte)을 구한다.
-func CreateStartByteAndEndByte(query QueryObj) ([]byte, []byte) {
-	salt := make([]byte, 2)
-	binary.BigEndian.PutUint16(salt, 0x0000)
-	startKeyObj := KeyObj{Timestamp: query.Start, Salt: salt}
-	endKeyObj := KeyObj{Timestamp: query.End, Salt: salt}
-
-	startByte, err := json.Marshal(startKeyObj)
-	if err != nil {
-		fmt.Println(err)
-	}
-	endByte, err := json.Marshal(endKeyObj)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	return startByte, endByte
-
 }
